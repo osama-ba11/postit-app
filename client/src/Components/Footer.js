692f0519d49afeb8e1388db8431b7ea769f49bd0
osama-ba11 <o7baomar@gmail.com>
2025-03-03 12:10:25 +0400

    update footer

--- client/src/Components/Footer.js
+++ client/src/Components/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="footer">
-      <div>©2023.PostIT. All Rights Reserved.</div>
+      <div>46s2015 - Osama Ba Omar</div>
     </footer>
   );
 };
