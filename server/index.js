5167e0959f8c65f3fa549060ed422bdeaf985726
osama-ba11 <o7baomar@gmail.com>
2025-04-21 13:41:13 +0400

    savePost API done

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import express from "express";
 import UserModel from "./Models/UserModel.js";
 import bcrypt from "bcrypt";
+import PostModel from "./Models/PostModel.js";
 
 const app = express();
 app.use(express.json());
@@ -10,7 +11,7 @@ app.use(cors());
 
 //Database connection
 const connectString =
-  "mongodb+srv://admin:admin12345@postitcluster.vcbpdnh.mongodb.net/postITDb?retryWrites=true&w=majority&appName=PostITCluster";
+  "mongodb+srv://admin:admin@postitcluster.z9nukur.mongodb.net/?retryWrites=true&w=majority&appName=PostITCluster";
 
 mongoose.connect(connectString);
 //API Routes
@@ -64,7 +65,27 @@ app.post("/logout", async (req, res) => {
 });
 
 app.put("/updateProfile", async (req, res) => {});
+//POST API - savePost
 
+app.post("/savePost", async (req, res) => {
+  try {
+    const postMsg = req.body.postMsg;
+
+    const email = req.body.email;
+
+    const post = new PostModel({
+      postMsg: postMsg,
+
+      email: email,
+    });
+
+    await post.save();
+
+    res.send({ post: post, msg: "Added." });
+  } catch (error) {
+    res.status(500).json({ error: "An error occurred" });
+  }
+});
 app.listen(3001, () => {
   console.log("You are connected thank you");
 });
